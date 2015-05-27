805f0d4f64e554a65cb5260cfbc20271ed629715
Sam Penrose <spenrose@mozilla.com>
2015-05-27 08:41:43 -0700

    wip

--- tests/testthat/test_activity.R
+++ tests/testthat/test_activity.R
@@ -1,7 +1,19 @@
 source("../../lib/activity.R")
 
+test_that("allActivity works", {
+    expect_equal(allActivity(NULL), NULL)
+})
+
+## Add Date objects
+test_that("last.month.day works", {
+    dates = c("2014-12-31", "2015-01-01", "2016-02-26")
+    expect_equal(last.month.day(dates),
+                 c("2014-12-31", "2015-01-31", "2016-02-29"))
+})
+
+## Add Date objects
 test_that("is.weekday works", {
-  days <- c("2015-05-26", "2015-05-24")
-  expect_equal(is.weekday(days), c(TRUE, FALSE))
-  expect_equal(is.weekday(c("")), c(FALSE))
+    days <- c("2015-05-26", "2015-05-24")
+    expect_equal(is.weekday(days), c(TRUE, FALSE))
+    expect_equal(is.weekday(c("")), c(FALSE))
 })

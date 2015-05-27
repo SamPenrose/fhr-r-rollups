5b883e6ef71152afbdfa5d3388137f0c70f62e5d
Sam Penrose <spenrose@mozilla.com>
2015-05-27 08:31:09 -0700

    wip

--- tests/testthat/test_activity.R
+++ tests/testthat/test_activity.R
@@ -1,8 +1,7 @@
 source("../../lib/activity.R")
 
 test_that("is.weekday works", {
-  expect_equal(2-1, 1)
-  days <- c("2015-05-26")
-
-  expect_equal(is.weekday(days), c(TRUE))
+  days <- c("2015-05-26", "2015-05-24")
+  expect_equal(is.weekday(days), c(TRUE, FALSE))
+  expect_equal(is.weekday(c("")), c(FALSE))
 })
